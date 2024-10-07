317113f400acc7a90fe0b3c611f5e7d3f15012ab
Shanilka <shanilka799@gmail.com>
2024-10-07 14:01:02 +0530

    add WebAppConfig class

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/config/WebAppConfig.java
@@ -0,0 +1,10 @@
+package lk.ijse.pos_system_backend_spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.pos_system_backend_spring")
+public class WebAppConfig {
+
+}
