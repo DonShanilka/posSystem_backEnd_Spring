8a181fc6516a248b01a441d5d9bd4364251185cb
Shanilka <shanilka799@gmail.com>
2024-10-07 14:37:49 +0530

    update webAppRootConfing class

--- src/main/java/lk/ijse/pos_system_backend_spring/config/WebAppRootConfig.java
+++ src/main/java/lk/ijse/pos_system_backend_spring/config/WebAppRootConfig.java
@@ -3,14 +3,22 @@ package lk.ijse.pos_system_backend_spring.config;
 import jakarta.persistence.EntityManagerFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.pos_system_backend_spring")
+@EnableJpaRepositories(basePackages = "lk.ijse.pos_system_backend_spring")
+@EnableTransactionManagement
 public class WebAppRootConfig {
     @Bean
     public ModelMapper modelMapper() {
