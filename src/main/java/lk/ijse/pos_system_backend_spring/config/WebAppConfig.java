15ea572cff3d12a1f084d961a94c8b00573e26b9
Shanilka <shanilka799@gmail.com>
2024-10-07 14:46:25 +0530

    update dependency and webAppConfig class

--- src/main/java/lk/ijse/pos_system_backend_spring/config/WebAppConfig.java
+++ src/main/java/lk/ijse/pos_system_backend_spring/config/WebAppConfig.java
@@ -2,8 +2,10 @@ package lk.ijse.pos_system_backend_spring.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "lk.ijse.pos_system_backend_spring")
 public class WebAppConfig {
 
