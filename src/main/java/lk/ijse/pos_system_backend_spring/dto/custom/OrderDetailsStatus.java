f5ed8609f10fac257441f60d680c361839fc8898
Shanilka <shanilka799@gmail.com>
2024-10-08 15:43:32 +0530

    finished Status Package

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/dto/custom/OrderDetailsStatus.java
@@ -0,0 +1,8 @@
+package lk.ijse.pos_system_backend_spring.dto.custom;
+
+import lk.ijse.pos_system_backend_spring.dto.SuperDto;
+
+import java.io.Serializable;
+
+public interface OrderDetailsStatus extends Serializable,SuperDto {
+}
