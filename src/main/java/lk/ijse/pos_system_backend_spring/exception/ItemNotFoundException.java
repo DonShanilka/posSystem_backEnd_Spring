ee445ae3f5b671ab10fbee58d0234dce69422fe3
Shanilka <shanilka799@gmail.com>
2024-10-10 10:12:15 +0530

    finished ItemNotFoundException

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/exception/ItemNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.pos_system_backend_spring.exception;
+
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
+
+    public ItemNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ItemNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
