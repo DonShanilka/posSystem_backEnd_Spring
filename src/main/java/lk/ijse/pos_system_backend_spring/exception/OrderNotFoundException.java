6488f4b6a51ba728873615b32e6f7dbb1d7ddda7
Shanilka <shanilka799@gmail.com>
2024-10-10 10:28:56 +0530

    finished OrderNotFoundException

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/exception/OrderNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.pos_system_backend_spring.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+    public OrderNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public OrderNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
