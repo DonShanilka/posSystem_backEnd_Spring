6876d0b6bc25c0a1aab20956fb4fe76d373fd569
Shanilka <shanilka799@gmail.com>
2024-10-10 09:40:16 +0530

    finished UserDao

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/dao/UserDao.java
@@ -0,0 +1,9 @@
+package lk.ijse.pos_system_backend_spring.dao;
+
+import lk.ijse.pos_system_backend_spring.entity.impl.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDao extends JpaRepository<User,String> {
+}
