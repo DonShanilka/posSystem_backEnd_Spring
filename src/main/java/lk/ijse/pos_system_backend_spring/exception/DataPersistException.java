73368177991318f76964dadee12c0a80ea222acd
Shanilka <shanilka799@gmail.com>
2024-10-10 10:02:02 +0530

    finished DataPersistException

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/exception/DataPersistException.java
@@ -0,0 +1,12 @@
+package lk.ijse.pos_system_backend_spring.exception;
+
+public class DataPersistException extends RuntimeException{
+    public DataPersistException() {
+    }
+
+    public DataPersistException(String message) {
+    }
+
+    public DataPersistException(Throwable cause) {
+    }
+}
