f8cd2b2154b9c88ebc91dded7572f770840d8d05
Shanilka <shanilka799@gmail.com>
2024-10-10 12:16:48 +0530

    finished UserNotFoundException

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package lk.ijse.pos_system_backend_spring.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
