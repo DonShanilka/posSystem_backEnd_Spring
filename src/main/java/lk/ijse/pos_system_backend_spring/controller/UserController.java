327dc5abaf3cd69ee8e3f6175d6c7fdf46d00160
Shanilka <shanilka799@gmail.com>
2024-10-11 11:13:50 +0530

    finished Controller

---
+++ src/main/java/lk/ijse/pos_system_backend_spring/controller/UserController.java
@@ -0,0 +1,75 @@
+package lk.ijse.pos_system_backend_spring.controller;
+
+import lk.ijse.pos_system_backend_spring.dto.custom.impl.UserDto;
+import lk.ijse.pos_system_backend_spring.exception.UserNotFoundException;
+import lk.ijse.pos_system_backend_spring.service.UserService;
+import lk.ijse.pos_system_backend_spring.util.RegexUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> saveUser(@RequestBody UserDto userDto) {
+        try {
+            userService.saveUser(userDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> getUserById(@PathVariable("userId") String userId) {
+        if (!RegexUtil.isValidUserId(userId)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try {
+            UserDto userDto = userService.getUserById(userId);
+            return new ResponseEntity<>(userDto, HttpStatus.OK);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserDto> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping(value = "/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateUser(@PathVariable("userId") String userId, @RequestBody UserDto userDto) {
+        if (!RegexUtil.isValidUserId(userId)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try {
+            userService.updateUser(userId, userDto);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("userId") String userId) {
+        if (!RegexUtil.isValidUserId(userId)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try {
+            userService.deleteUser(userId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
