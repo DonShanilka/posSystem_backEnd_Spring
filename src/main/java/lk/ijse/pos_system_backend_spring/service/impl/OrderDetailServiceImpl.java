47d5807543356916f529ab13138c96826824ebaa
Shanilka <shanilka799@gmail.com>
2024-10-17 12:38:33 +0530

    update saveOrderDetail in itemserrviceImpl class

--- src/main/java/lk/ijse/pos_system_backend_spring/service/impl/OrderDetailServiceImpl.java
+++ src/main/java/lk/ijse/pos_system_backend_spring/service/impl/OrderDetailServiceImpl.java
@@ -1,16 +1,33 @@
 package lk.ijse.pos_system_backend_spring.service.impl;
 
+import lk.ijse.pos_system_backend_spring.dao.OrderDetailDao;
 import lk.ijse.pos_system_backend_spring.dto.custom.impl.OrderDetailsDto;
+import lk.ijse.pos_system_backend_spring.entity.impl.OrderDetail;
+import lk.ijse.pos_system_backend_spring.exception.DataPersistException;
 import lk.ijse.pos_system_backend_spring.service.OrderDetailsService;
+import lk.ijse.pos_system_backend_spring.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OrderDetailServiceImpl implements OrderDetailsService {
+
+    @Autowired
+    private OrderDetailDao orderDetailDao;
+
+    @Autowired
+    private Mapping mapping;
+
     @Override
     public void saveOrderDetail(OrderDetailsDto orderDetailDto) {
+        OrderDetail orderDetail = mapping.toOrderDetailEntity(orderDetailDto);
 
+        OrderDetail savedOrderDetail = orderDetailDao.save(orderDetail);
+        if (savedOrderDetail == null) {
+            throw new DataPersistException("OrderDetail not saved");
+        }
     }
 
     @Override
